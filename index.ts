9f4ad9d910a803621165a4dea4617b5b4d971bb2
rycont <reactdev@kakao.com>
2020-08-07 11:59:23 +0900

    feat : Runnable

---
+++ index.ts
@@ -0,0 +1,28 @@
+import "https://deno.land/x/dotenv/load.ts";
+  
+;(async () => {
+  const [WakaTimeURI, GistId, GithubToken] = ['WakaTimeURI', 'GistId', 'GithubToken'].map(Deno.env.get)
+  if (!(WakaTimeURI && GistId && GithubToken)) throw "Requierd env was not provided"
+  
+  const fetched = (await (await fetch(WakaTimeURI)).json()).data
+  console.log(fetched)
+  const printified = fetched.map((e: {
+    name: string,
+    percent: number
+  }) => `${e.name.padEnd(10, ' ')} : ${String(e.percent).padStart(6)}% ${'■'.repeat(Math.ceil(e.percent))}`).join('\n')
+  fetch(`https://api.github.com/gists/${GistId}`, {
+    headers: {
+      'Authorization': `token ${GithubToken}`, 
+    },
+    body: JSON.stringify({
+      "files": {
+          "I Code...": {
+          "content": `${printified}
+          
+Created from Deno with Github Actions!`
+          }
+      }
+    }),
+    method: 'PATCH'
+  })
+})()
